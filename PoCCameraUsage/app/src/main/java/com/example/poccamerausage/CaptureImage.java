5d81251fdb9a39ceaa56e803366a3d61316ede40
Kenyon Bunker <kjbunker817@gmail.com>
2019-05-31 15:57:49 -0700

    PoC update

--- PoCCameraUsage/app/src/main/java/com/example/poccamerausage/CaptureImage.java
+++ PoCCameraUsage/app/src/main/java/com/example/poccamerausage/CaptureImage.java
@@ -1,38 +1,85 @@
 package com.example.poccamerausage;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
+import android.media.MediaRecorder;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
-import android.provider.MediaStore;
-import android.support.v4.content.FileProvider;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.view.SurfaceHolder;
+import android.util.Log;
+import android.view.SurfaceView;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.Toast;
+import android.widget.FrameLayout;
+import android.hardware.Camera.PictureCallback;
+
+
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE;
+
 public class CaptureImage extends AppCompatActivity {
 
-    private static final int REQUEST_TAKE_PHOTO = 0;
-    private String currentPhotoPath;
-    private ImageView mImageView;
-    private static final int REQUEST_IMAGE_CAPTURE = 101;
+    private static final String TAG = "CaptureImage";
+    private Camera mCamera;
+    private CameraPreview mPreview;
+    private SurfaceView preview;
+    private MediaRecorder mediaRecorder;
+    View view = null;
+    private PictureCallback picture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        mImageView = findViewById(R.id.imageView);
+        setContentView(R.layout.capture_image);
+
+        if (ContextCompat.checkSelfPermission(CaptureImage.this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission is granted
+
+            ActivityCompat.requestPermissions(CaptureImage.this,
+                    new String[]{Manifest.permission.CAMERA},
+                    0);
+            Log.d(TAG, "Permission for camera granted");
+        }
+
+            // Create an instance of Camera
+            mCamera = getCameraInstance();
+            Log.d(TAG, "Camera instance created");
+
+            // Create our Preview view and set it as the content of our activity.
+
+            mPreview = new CameraPreview(this, mCamera);
+
+            FrameLayout preview = (FrameLayout)findViewById(R.id.camera_preview);
+            preview.addView(preview);
+
+            // Add a listener to the Capture button
+            Button captureButton = (Button) findViewById(R.id.btnCaptureImage);
+            captureButton.setOnClickListener(
+                    new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            // get an image from the camera
+                            mCamera.takePicture(null, null, getOutputMediaFile());
+                            Log.d(TAG, "Button Clicked");
+                        }
+                    }
+            );
+
 
     }
 
@@ -47,59 +94,114 @@ public class CaptureImage extends AppCompatActivity {
     protected void onPause(){
         //TODO: Release camera
         super.onPause();
+        releaseMediaRecorder();       // if you are using MediaRecorder, release it first
+        releaseCamera();              // release the camera immediately on pause event
 
     }
 
-    public void captureImage(View view) {
-        Intent imageTakeIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
-        if (imageTakeIntent.resolveActivity(getPackageManager())!=null) {
-            
+    /** Check if this device has a camera */
+    private boolean checkCameraHardware(Context context) {
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+            // this device has a camera
+            return true;
+        } else {
+            // no camera on this device
+            return false;
         }
     }
 
-
-    //Creates image file and folder structure
-    private File createImageFile() throws IOException {
-        // Create an image file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + "_";
-
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        //Note: Files you save in the directories provided by getExternalFilesDir() or
-        // getFilesDir() are deleted when the user uninstalls the app.
-        File image = File.createTempFile(
-                imageFileName,  /* prefix */
-                ".jpg",         /* suffix */
-                storageDir      /* directory */
-        );
-        // Save a file: path for use with ACTION_VIEW intents
-        currentPhotoPath = image.getAbsolutePath();
-        return image;
+    /** A safe way to get an instance of the Camera object. */
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
     }
 
-    //takes the picture
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        // Ensure that there's a camera activity to handle the intent
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            // Create the File where the photo should go
-            File photoFile = null;
+    //Creates the picture file
+    private Camera.PictureCallback mPicture = new PictureCallback() {
+
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+
+            File pictureFile = getOutputMediaFile(MEDIA_TYPE_IMAGE);
+            if (pictureFile == null){
+                Log.d(TAG, "Error creating media file, check storage permissions");
+                return;
+            }
+
             try {
-                photoFile = createImageFile();
-            } catch (IOException ex) {
-                // Error occurred while creating the File
+                FileOutputStream fos = new FileOutputStream(pictureFile);
+                fos.write(data);
+                fos.close();
+            } catch (FileNotFoundException e) {
+                Log.d(TAG, "File not found: " + e.getMessage());
+            } catch (IOException e) {
+                Log.d(TAG, "Error accessing file: " + e.getMessage());
             }
-            // Continue only if the File was successfully created
-            if (photoFile != null) {
-                Uri photoURI = FileProvider.getUriForFile(this,
-                        "com.example.android.fileprovider",
-                        photoFile);
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+        }
+    };
+
+    public static final int MEDIA_TYPE_IMAGE = 1;
+    public static final int MEDIA_TYPE_VIDEO = 2;
+
+    /** Create a file Uri for saving an image or video */
+    private static Uri getOutputMediaFileUri(int type){
+        return Uri.fromFile(getOutputMediaFile(type));
+    }
+
+    /** Create a File for saving an image or video */
+    private static File getOutputMediaFile(int type){
+        // To be safe, you should check that the SDCard is mounted
+        // using Environment.getExternalStorageState() before doing this.
+
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "MyCameraApp");
+        // This location works best if you want the created images to be shared
+        // between applications and persist after your app has been uninstalled.
+
+        // Create the storage directory if it does not exist
+        if (! mediaStorageDir.exists()){
+            if (! mediaStorageDir.mkdirs()){
+                Log.d("MyCameraApp", "failed to create directory");
+                return null;
             }
+        }
+
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE){
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "IMG_"+ timeStamp + ".jpg");
+        } else if(type == MEDIA_TYPE_VIDEO) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "VID_"+ timeStamp + ".mp4");
+        } else {
+            return null;
+        }
+
+        return mediaFile;
+    }
+
+    private void releaseMediaRecorder(){
+        if (mediaRecorder != null) {
+            mediaRecorder.reset();   // clear recorder configuration
+            mediaRecorder.release(); // release the recorder object
+            mediaRecorder = null;
+            mCamera.lock();           // lock camera for later use
         }
     }
 
-
+    // release the camera for other applications
+    private void releaseCamera(){
+        if (mCamera != null){
+            mCamera.release();
+            mCamera = null;
+        }
+    }
 }
--- PoCCameraUsage/app/src/main/java/com/example/poccamerausage/MainActivity.java
+++ PoCCameraUsage/app/src/main/java/com/example/poccamerausage/MainActivity.java
@@ -34,3 +34,13 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 }
+/*  TODO:   1. Detect and Access Camera - Create code to check for the existence of cameras and request access.
+    TODO:   2. Create a Preview Class - Create a camera preview class that extends SurfaceView and implements
+                the SurfaceHolder interface. This class previews the live images from the camera.
+    TODO:   3. Build a Preview Layout - Once you have the camera preview class, create a view layout that
+                incorporates the preview and the user interface controls you want.
+    TODO:   4. Setup Listeners for Capture - Connect listeners for your interface controls to start image or video
+                capture in response to user actions, such as pressing a button.
+    TODO:   5. Capture and Save Files - Setup the code for capturing pictures or videos and saving the output.
+    TODO:   6. Release the Camera - After using the camera, your application must properly release it for use
+                by other applications.*/
